52f3f3b37af1012f76d3cac4e372d500829e84ac
Mark Goldfarb <markgoldfarb@Marks-MacBook-Air.local>
2015-03-02 15:48:10 -0700

    add has_many relationship to categories table and has_many through relationship to kittens table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ActiveRecord::Base
   validates :name, presence: true, uniqueness: {case_sensitive: false}
+
+  has_many :categorizations
+  has_many :kittens, through: :categorizations
 end
