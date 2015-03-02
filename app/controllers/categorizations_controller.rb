661a1d6c6e227c8bd2b4b4b67b35cd848d389702
Mark Goldfarb <markgoldfarb@Marks-MacBook-Air.local>
2015-03-02 15:44:31 -0700

    add create and new methods

--- app/controllers/categorizations_controller.rb
+++ app/controllers/categorizations_controller.rb
@@ -9,4 +9,25 @@ class CategorizationsController < ApplicationController
     @categorization = Categorization.new
   end
 
+  def new
+    @categorization = Categorization.new
+    @kitten = Kitten.find(params[:kitten_id])
+  end
+
+  def create
+    @categorization = Categorization.new(categorization_params)
+    @categorization.kitten_id = params[:kitten_id]
+    if @categorization.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def categorization_params
+    params.require(:categorization).permit(:category_id, :kitten_id)
+  end
+
 end
