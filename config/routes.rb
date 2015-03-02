a37ac2456a7fad3ee16755cda6708ac3801a0a2f
Mark Goldfarb <markgoldfarb@Marks-MacBook-Air.local>
2015-03-02 14:08:27 -0700

    remove resources for kittens and categories and add resources for join(categories_kittens)

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   get "signout" => "sessions#destroy", as: :signout
   get "/categories" => "categories#index", as: :categories
 
-  resources :kittens do
-    resources :categories
-  end
+  resources :categories_kittens
 
 end
